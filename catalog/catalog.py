c6692770f63a3325e0bdb92ee1e95868be9f777c
John Lynn <john@silferein.com>
2016-01-18 21:31:15 -0800

    Update item pages with category support.

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, g, request, flash, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
-from database_setup import Item
+from database_setup import Item, Category
 
 app = Flask(__name__)
 
@@ -29,27 +29,39 @@ def teardown_request(exception):
 def index():
 	return render_template("index.html")
 
-@app.route('/catalog/items/', methods=["GET", "POST", "DELETE"])
+@app.route('/catalog/items/', methods=["GET", "POST"])
 def item_list():
     if request.method == 'POST':
         # Read the post
         name = request.form['name']
         description = request.form['description']
+        category_id = request.form['category_id']
         # Handle update requests
         if 'id' in request.form:
             id = request.form['id']
-            g.s.query(Item).filter(Item.id==id)\
-                .update({"name":name, "description":description})
+            g.s.query(Item).filter(Item.id==id).update(
+                {"name":name, 
+                "description":description, 
+                "category_id":category_id})
             g.s.commit()
             flash("'%s' has been updated succesfully!" % name)
         # Handle creation requests
         else:
-            g.s.add(Item(name=name, description=description))
+            g.s.add(
+                Item(
+                    name=name, 
+                    description=description, 
+                    category_id=category_id))
             g.s.commit()
             flash("'%s' has been created succesfully!" % name)
     # Load the page
     items = g.s.query(Item).all()
     return render_template('list_items.html', items=items)
+    
+@app.route('/catalog/items/add/')
+def item_add():
+    categories = g.s.query(Category).all()
+    return render_template('add_item.html', categories=categories)
     
 @app.route('/catalog/items/<int:item_id>', methods=['GET'])
 def item_view(item_id):
@@ -59,7 +71,8 @@ def item_view(item_id):
 @app.route('/catalog/items/<item_id>/edit')
 def item_edit(item_id):
     item = g.s.query(Item).filter(Item.id == item_id).first()
-    return render_template('edit_item.html', item=item)
+    categories = g.s.query(Category).all()
+    return render_template('edit_item.html', item=item, categories=categories)
 	
 @app.route('/catalog/items/<item_id>/delete', methods=['GET','POST'])
 def item_delete(item_id):
